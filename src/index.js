03e659ed860781aa516020eb01e74fa3ec24efcb
CoderGuy614 <jlutz.110@gmail.com>
2020-07-14 11:53:59 +0800

    Add redux perist to persist cart items across sessions

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./redux/store/store";
+import { store, persistor } from "./redux/store/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
 
