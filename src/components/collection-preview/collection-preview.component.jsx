0a9ce0e69d033794377a71305ec0da209eebe44d
CoderGuy614 <jlutz.110@gmail.com>
2020-07-16 17:57:19 +0800

    Create Collection Preview Component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,7 +4,7 @@ import "./collection-preview.styles.scss";
 
 import CollectionItem from "../collection-item/collection-item.component";
 
-const CollectionPreview = ({ title, items }) => (
+export const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
@@ -16,5 +16,3 @@ const CollectionPreview = ({ title, items }) => (
     </div>
   </div>
 );
-
-export default CollectionPreview;
--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,21 +1,11 @@
 import React from "react";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-
-import { selectCollections } from "../../redux/shop/shop.selectors";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
 const ShopPage = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...rest }) => (
-      <CollectionPreview key={id} {...rest} />
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
