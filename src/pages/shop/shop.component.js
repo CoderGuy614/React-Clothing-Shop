41425b66634f89d7c8b3bf168fc31c7f2e150b41
CoderGuy614 <jlutz.110@gmail.com>
2020-07-16 17:28:15 +0800

    Move Shop Data To Global State

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,26 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-import SHOP_DATA from "./shop.data";
+import { selectCollections } from "../../redux/shop/shop.selectors";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...rest }) => (
-          <CollectionPreview key={id} {...rest} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...rest }) => (
+      <CollectionPreview key={id} {...rest} />
+    ))}
+  </div>
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
